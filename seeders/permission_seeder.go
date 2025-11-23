2225bb5b02304bf85d455fa256505834b22ae47e
alfarezyyd <adityaalfarezyd@gmail.com>
2025-11-23 17:18:29 +0700

    feat(seeder): Add permission seeder

---
+++ seeders/permission_seeder.go
@@ -0,0 +1,39 @@
+package seeders
+
+import (
+	"fmt"
+	"go-intconnect-api/internal/entity"
+	"strings"
+
+	"github.com/jaswdr/faker/v2"
+	"gorm.io/gorm"
+)
+
+type PermissionSeeder struct{}
+
+func (permissionSeeder *PermissionSeeder) Run(gormDatabase *gorm.DB) error {
+	fakerInstance := faker.New()
+
+	categories := []string{"machine", "facility", "user", "auth", "role"}
+
+	for i := 0; i < 10; i++ {
+		randomWord := fakerInstance.Lorem().Word()
+		randomCategory := fakerInstance.RandomStringElement(categories)
+
+		permissionEntity := entity.Permission{
+			Code:        fmt.Sprintf("perm_%s_%d", randomCategory, fakerInstance.Int64Between(1, 9999)),
+			Name:        strings.Title(fmt.Sprintf("%s %s", randomCategory, randomWord)),
+			Category:    randomCategory,
+			Description: fakerInstance.Lorem().Sentence(10),
+			Auditable:   entity.NewAuditable("Admin"),
+		}
+
+		if err := gormDatabase.Create(&permissionEntity).Error; err != nil {
+			return err
+		}
+
+		fmt.Println("Inserted permission:", permissionEntity.Code)
+	}
+
+	return nil
+}
---
+++ seeders/registry.go
@@ -0,0 +1,18 @@
+package seeders
+
+import (
+	"errors"
+)
+
+// Map nama seeder → struct
+var SeederRegistry = map[string]Seeder{
+	"PermissionSeeder": &PermissionSeeder{},
+}
+
+// Ambil seeder dari nama
+func GetSeeder(name string) (Seeder, error) {
+	if seeder, ok := SeederRegistry[name]; ok {
+		return seeder, nil
+	}
+	return nil, errors.New("Seeder not found: " + name)
+}
