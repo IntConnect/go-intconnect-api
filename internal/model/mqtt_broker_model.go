a63d8cc0683598702237aef02b20b62ded2ddbe5
alfarezyyd <adityaalfarezyd@gmail.com>
2025-11-26 19:35:26 +0700

    feat(mqtt-broker): Change gorm to json

--- internal/model/mqtt_broker_model.go
+++ internal/model/mqtt_broker_model.go
@@ -2,19 +2,19 @@ package model
 
 type MqttBrokerResponse struct {
 	Id       uint64 `json:"id"`
-	HostName string `gorm:"json:host_name"`
-	MqttPort int    `gorm:"json:mqtt_port"`
-	WsPort   int    `gorm:"json:ws_port"`
-	Username string `gorm:"json:username"`
-	Password string `gorm:"json:password"`
-	IsActive bool   `gorm:"json:is_active"`
+	HostName string `json:"host_name"`
+	MqttPort int    `json:"mqtt_port"`
+	WsPort   int    `json:"ws_port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	IsActive bool   `json:"is_active"`
 	AuditableResponse
 }
 
 type CreateMqttBrokerRequest struct {
-	HostName string `json:"host_name" validate:"required"`
-	MqttPort string `json:"mqtt_port" validate:"required"`
-	WsPort   string `json:"ws_port" validate:"required"`
+	HostName string `json:"host_name" validate:"required,min=3,max=100"`
+	MqttPort string `json:"mqtt_port" validate:"required,min=1,max=5"`
+	WsPort   string `json:"ws_port" validate:"required,min=1,max=5"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	IsActive bool   `json:"is_active" validate:"required"`
