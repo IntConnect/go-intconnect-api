1154dc37344fd14941acc2d5858a557be6f4747b
alfarezyyd <adityaalfarezyd@gmail.com>
2025-11-28 08:58:45 +0700

    feat(helper,model): - Add reason to model - helper Extract JWT

--- internal/model/general_model.go
+++ internal/model/general_model.go
@@ -1,5 +1,6 @@
 package model
 
 type DeleteResourceGeneralRequest struct {
-	Id uint64 `json:"id" validate:"required|number"`
+	Id     uint64 `json:"id" validate:"required|number"`
+	Reason string `json:"reason" validate:"required"`
 }
--- pkg/helper/general_helper.go
+++ pkg/helper/general_helper.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"go-intconnect-api/internal/model"
 	"go-intconnect-api/pkg/exception"
 	"mime/multipart"
 	"net/http"
@@ -33,7 +34,7 @@ func ExtractIndexedFiles(
 
 	multipartForm, err := ginContext.MultipartForm()
 	CheckErrorOperation(err, exception.NewApplicationError(http.StatusBadRequest, exception.ErrBadRequest, err))
-	fileHeadersArray := make([]*multipart.FileHeader, expectedLen) // 🔥 pre-allocate
+	fileHeadersArray := make([]*multipart.FileHeader, expectedLen)
 
 	for key, fileHeaders := range multipartForm.File {
 		if strings.HasPrefix(key, prefixKey) &&
@@ -52,4 +53,16 @@ func ExtractIndexedFiles(
 	}
 
 	return fileHeadersArray, nil
+}
+
+func ExtractJwtClaimFromContext(ginContext *gin.Context) *model.JwtClaimRequest {
+	claims, exists := ginContext.Get("claims")
+	if !exists {
+		exception.ThrowApplicationError(exception.NewApplicationError(http.StatusUnauthorized, exception.ErrUnauthorized, nil))
+	}
+	userClaim, ok := claims.(*model.JwtClaimRequest)
+	if !ok {
+		exception.ThrowApplicationError(exception.NewApplicationError(http.StatusUnauthorized, exception.ErrUnauthorized, nil))
+	}
+	return userClaim
 }
