ac31e9442355e4a467ce451970982b16a7037316
alfarezyyd <adityaalfarezyd@gmail.com>
2025-11-30 10:53:06 +0700

    feat(schema): Add role_id user schema

--- migrations/20251120143733_create_telemetries_table.up.sql
+++ migrations/20251120143733_create_telemetries_table.up.sql
@@ -2,10 +2,11 @@ CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 CREATE TABLE telemetries
 (
+    id           BIGSERIAL NOT NULL,
     parameter_id BIGINT    NOT NULL REFERENCES parameters (id),
     value        FLOAT,
     timestamp    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id,timestamp)
+    PRIMARY KEY (id, timestamp)
 );
 
 SELECT create_hypertable(
