98a3093e5e7e25a63f72ff74e33e3267b20605f0
alfarezyyd <adityaalfarezyd@gmail.com>
2025-12-08 22:53:35 +0700

    feat(parameter-operations): Parameter operation schema

---
+++ migrations/20251208155051_create_parameter_operations_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE parameter_operations
+(
+    id           SERIAL PRIMARY KEY NOT NULL,
+    type         VARCHAR(255) DEFAULT '',
+    value        FLOAT,
+    sequence     INT,
+    parameter_id BIGINT             NOT NULL REFERENCES parameters (id)
+
+)
