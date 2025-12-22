505185c44f762b985d65f0cc4cff0de25db56cea
alfarezyyd <adityaalfarezyd@gmail.com>
2025-12-22 14:49:13 +0700

    feat(seeder): Seeder update - Add initial seeder - Add permission seeder

---
+++ seeders/initial_seeder.go
@@ -0,0 +1,28 @@
+package seeders
+
+import (
+	"log"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type InitialSeeder struct{}
+
+func (initialSeeder *InitialSeeder) Run(gormDatabase *gorm.DB) error {
+	seederNames := []string{"RoleSeeder", "PermissionSeeder", "RolePermissionSeeder", "UserSeeder"}
+	for _, name := range seederNames {
+		s, err := GetSeeder(name)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		logrus.Debug("Running seeder:", name)
+		if err := s.Run(gormDatabase); err != nil {
+			log.Println("Seeder failed:", err)
+		} else {
+			logrus.Debug("Seeder completed:", name)
+		}
+	}
+	return nil
+}
--- seeders/permission_seeder.go
+++ seeders/permission_seeder.go
@@ -130,63 +130,33 @@ func (permissionSeeder *PermissionSeeder) Run(gormDatabase *gorm.DB) error {
 			Auditable:   entity.NewAuditable("System"),
 		},
 
-		// Role
+		// Role & User
 		{
-			Code:        "ROLE_VIEW",
+			Code:        "ROLE_USER_VIEW",
 			Name:        "View",
-			Category:    "Role",
-			Description: "Permission to view roles",
+			Category:    "Role & User",
+			Description: "Permission to view roles & users",
 			Auditable:   entity.NewAuditable("System"),
 		},
 		{
-			Code:        "ROLE_CREATE",
+			Code:        "ROLE_USER_CREATE",
 			Name:        "Create",
-			Category:    "Role",
-			Description: "Permission to create roles",
+			Category:    "Role & User",
+			Description: "Permission to create roles & users",
 			Auditable:   entity.NewAuditable("System"),
 		},
 		{
-			Code:        "ROLE_EDIT",
+			Code:        "ROLE_USER_EDIT",
 			Name:        "Update",
-			Category:    "Role",
-			Description: "Permission to update roles",
+			Category:    "Role & User",
+			Description: "Permission to update roles & users",
 			Auditable:   entity.NewAuditable("System"),
 		},
 		{
-			Code:        "ROLE_DELETE",
+			Code:        "ROLE_USER_DELETE",
 			Name:        "Delete",
-			Category:    "Role",
-			Description: "Permission to delete roles",
-			Auditable:   entity.NewAuditable("System"),
-		},
-
-		// User
-		{
-			Code:        "USER_VIEW",
-			Name:        "View",
-			Category:    "User",
-			Description: "Permission to view users",
-			Auditable:   entity.NewAuditable("System"),
-		},
-		{
-			Code:        "USER_CREATE",
-			Name:        "Create",
-			Category:    "User",
-			Description: "Permission to create users",
-			Auditable:   entity.NewAuditable("System"),
-		},
-		{
-			Code:        "USER_EDIT",
-			Name:        "Update",
-			Category:    "User",
-			Description: "Permission to update users",
-			Auditable:   entity.NewAuditable("System"),
-		},
-		{
-			Code:        "USER_DELETE",
-			Name:        "Delete",
-			Category:    "User",
-			Description: "Permission to delete users",
+			Category:    "Role & User",
+			Description: "Permission to delete roles & users",
 			Auditable:   entity.NewAuditable("System"),
 		},
 
@@ -217,6 +187,139 @@ func (permissionSeeder *PermissionSeeder) Run(gormDatabase *gorm.DB) error {
 			Name:        "Delete",
 			Category:    "Facility",
 			Description: "Permission to delete facilities",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// BREAKDOWN
+		{
+			Code:        "BREAKDOWN_VIEW",
+			Name:        "View",
+			Category:    "Breakdown",
+			Description: "Permission to view breakdowns",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "BREAKDOWN_CREATE",
+			Name:        "Create",
+			Category:    "Breakdown",
+			Description: "Permission to create breakdowns",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "BREAKDOWN_EDIT",
+			Name:        "Update",
+			Category:    "Breakdown",
+			Description: "Permission to update breakdowns",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "BREAKDOWN_DELETE",
+			Name:        "Delete",
+			Category:    "Breakdown",
+			Description: "Permission to delete breakdowns",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// LOG ALARM
+		{
+			Code:        "LOG_ALARM_VIEW",
+			Name:        "View",
+			Category:    "Log Alarm",
+			Description: "Permission to view log alarm",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// Report Document Template
+		{
+			Code:        "REPORT_DOCUMENT_TEMPLATE_VIEW",
+			Name:        "View",
+			Category:    "Report Document Template",
+			Description: "Permission to view report document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "REPORT_DOCUMENT_TEMPLATE_CREATE",
+			Name:        "Create",
+			Category:    "Report Document Template",
+			Description: "Permission to create report document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "REPORT_DOCUMENT_TEMPLATE_EDIT",
+			Name:        "Update",
+			Category:    "Report Document Template",
+			Description: "Permission to update report document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "REPORT_DOCUMENT_TEMPLATE_DELETE",
+			Name:        "Delete",
+			Category:    "Report Document Template",
+			Description: "Permission to delete report document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// Generate Report
+		{
+			Code:        "GENERATE_REPORT_VIEW",
+			Name:        "View",
+			Category:    "Generate Report",
+			Description: "Permission to view generate reports",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// Check Sheet Document Template
+		{
+			Code:        "CHECK_SHEET_DOCUMENT_TEMPLATE_VIEW",
+			Name:        "View",
+			Category:    "Check Sheet Document Template",
+			Description: "Permission to view check sheet document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "CHECK_SHEET_DOCUMENT_TEMPLATE_CREATE",
+			Name:        "Create",
+			Category:    "Check Sheet Document Template",
+			Description: "Permission to create check sheet document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "CHECK_SHEET_DOCUMENT_TEMPLATE_EDIT",
+			Name:        "Update",
+			Category:    "Check Sheet Document Template",
+			Description: "Permission to update check sheet document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "CHECK_SHEET_DOCUMENT_TEMPLATE_DELETE",
+			Name:        "Delete",
+			Category:    "Check Sheet Document Template",
+			Description: "Permission to delete check sheet document templates",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// Check Sheet
+		{
+			Code:        "CHECK_SHEET_VIEW",
+			Name:        "View",
+			Category:    "Check Sheet",
+			Description: "Permission to view check sheet",
+			Auditable:   entity.NewAuditable("System"),
+		},
+
+		// System Setting
+		{
+			Code:        "SYSTEM_SETTING_VIEW",
+			Name:        "View",
+			Category:    "System Setting",
+			Description: "Permission to view system setting",
+			Auditable:   entity.NewAuditable("System"),
+		},
+		{
+			Code:        "SYSTEM_SETTING_MANAGE",
+			Name:        "Manage",
+			Category:    "System Setting",
+			Description: "Permission to manage system setting",
 			Auditable:   entity.NewAuditable("System"),
 		},
 	})
