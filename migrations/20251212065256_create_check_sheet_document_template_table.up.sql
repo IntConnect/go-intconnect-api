87402e07f604e0f35747ae6a555d4a7fc115e1cb
alfarezyyd <adityaalfarezyd@gmail.com>
2025-12-24 17:47:30 +0700

    feat(check-sheet-document-templates):Change schema

--- migrations/20251212065256_create_check_sheet_document_template_table.up.sql
+++ migrations/20251212065256_create_check_sheet_document_template_table.up.sql
@@ -1,13 +1,19 @@
 CREATE TABLE check_sheet_document_templates
 (
-    id               SERIAL PRIMARY KEY,
-    name             VARCHAR(255) NOT NULL,
-    code             VARCHAR(255) NOT NULL,
-    document_version INT          NOT NULL DEFAULT 0,
-    created_by       VARCHAR(255),
-    created_at       TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
-    updated_at       TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
-    updated_by       VARCHAR(255),
-    deleted_at       TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
-    deleted_by       VARCHAR(255)
+    id              SERIAL PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    no              VARCHAR(255) NOT NULL,
+    description     VARCHAR(255),
+    category        VARCHAR(255) NOT NULL,
+    rotation        INT          NOT NULL DEFAULT 1,
+    rotation_type   VARCHAR(255) NOT NULL DEFAULT 'Day',
+    interval        INT          NOT NULL DEFAULT 1,
+    revision_number INT          NOT NULL DEFAULT 0,
+    effective_date  DATE         NOT NULL,
+    created_by      VARCHAR(255),
+    created_at      TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    updated_by      VARCHAR(255),
+    deleted_at      TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    deleted_by      VARCHAR(255)
 )
