d74f1512fb52c67ebc0b653c27944db1a22bafa9
alfarezyyd <adityaalfarezyd@gmail.com>
2026-01-02 02:10:19 +0700

    fix(schema): change references

--- migrations/20251226124623_create_check_sheet_values_table.up.sql
+++ migrations/20251226124623_create_check_sheet_values_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE check_sheet_values
 (
     id             SERIAL PRIMARY KEY,
     check_sheet_id BIGINT       NOT NULL REFERENCES check_sheets (id),
-    parameter_id   BIGINT       NOT NULL REFERENCES check_sheet_document_templates_parameters (id),
+    parameter_id   BIGINT       NOT NULL REFERENCES parameters (id),
     timestamp      VARCHAR(255) NOT NULL,
     value          TEXT         NOT NULL
 )
