cd1747db4db5ebd5b63746cae66c1e823165ae27
alfarezyyd <adityaalfarezyd@gmail.com>
2026-01-02 19:51:27 +0700

    feat(schema): register schema

---
+++ migrations/20260102012006_create_registers_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE registers
+(
+    id               SERIAL PRIMARY KEY,
+    machine_id       BIGINT       NOT NULL REFERENCES machines (id),
+    modbus_server_id BIGINT       NOT NULL REFERENCES modbus_servers (id),
+    memory_location  VARCHAR(255) NOT NULL,
+    name             VARCHAR(255) NOT NULL,
+    description      VARCHAR(255) NOT NULL,
+    value            TEXT         NOT NULL DEFAULT 'String',
+    data_type        VARCHAR(255) NOT NULL,
+    created_at       TIMESTAMP    NOT NULL DEFAULT current_timestamp,
+    created_by       VARCHAR(255),
+    updated_at       TIMESTAMP    NOT NULL DEFAULT current_timestamp,
+    updated_by       VARCHAR(255),
+    deleted_at       TIMESTAMP,
+    deleted_by       VARCHAR(255)
+)
